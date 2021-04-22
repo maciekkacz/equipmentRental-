6b90fbdefa99f6fdf6aa5e2543d822dac7c42189
Maciek Kacz <maciek.kacz@wp.pl>
2021-04-22 22:43:35 +0200

    create new SQL

---
+++ src/main/java/DevicrentApp.java
@@ -0,0 +1,9 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DevicrentApp {
+    public static void main(String[] args) {
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("devicrentPU");
+        emFactory.close();
+    }
+}
